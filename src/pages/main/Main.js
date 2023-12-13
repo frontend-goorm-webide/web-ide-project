057648b482e303f6cf159e02e962f3e56a15d376
jhrogh <wogml5031@gmail.com>
2023-12-13 15:13:25 +0900

    Common Modal Components 초안

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import CommonModal from '../../components/Modal';
 
 function Main() {
-  return <div>Main</div>;
+  return (
+    <div>
+      Main
+      <CommonModal />
+    </div>
+  );
 }
 
 export default Main;
