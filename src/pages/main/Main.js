6234dd26409e73014a03f17d5a93754c824e229d
jhrogh <wogml5031@gmail.com>
2023-12-14 02:00:26 +0900

    main 버튼 두개 생성, 버튼에 따른 모달 띄우기, 모달에서 특정 페이지 이동하기

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,13 +1,57 @@
-import React from 'react';
-import CommonModal from '../../components/Modal';
+import React, { useState } from 'react';
 import Button from '../../components/Button';
+import CommonModal from '../../components/Modal';
 
 function Main() {
+  // 모달 열고 닫기 초기화
+  const [isModalOpen, setModalOpen] = useState(false);
+  // 모달 내용 초기화
+  const [modalContent, setModalContent] = useState({});
+
+  // 모달 열기 함수
+  const openModal = (content) => {
+    setModalOpen(true);
+    setModalContent(content);
+  };
+
+  // 모달 닫기 함수
+  const closeModal = () => {
+    setModalOpen(false);
+    setModalContent({});
+  };
+
   return (
     <div>
       Main
-      <CommonModal />
-      {/* <Button type='submit'>찾기</Button> */}
+      {/* 아이디찾기 버튼 */}
+      <Button
+        onClick={() =>
+          openModal({
+            title: '아이디 찾기',
+            contents: '해당 아이디로 다시 로그인 해주세요:)',
+            btnName: '로그인하기',
+            redirectTo: '/',
+          })
+        }
+      >
+        아이디 찾기
+      </Button>
+      {/* 비밀번호찾기 버튼 */}
+      <Button
+        onClick={() =>
+          openModal({
+            title: '비밀번호 찾기',
+            contents: '새로운 비밀번호를 설정하세요:)',
+            btnName: '확인',
+            redirectTo: '/ide',
+          })
+        }
+      >
+        비밀번호 찾기
+      </Button>
+      {/* isModalOpen 이 true 인 경우에만 && 뒤 실행
+      CommonModal 컴포넌트 랜더링, { }는 내용, closeModal에 닫기 함수 전달 */}
+      {isModalOpen && <CommonModal {...modalContent} closeModal={closeModal} />}
     </div>
   );
 }
