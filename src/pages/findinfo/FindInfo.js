7e1ba6f0cc101b299d63ad4baa5decea8ab1f8fb
Hyejung Na <hyejung.na01@gmail.com>
2023-12-14 13:07:20 +0900

    findinfo페이지 ui수정

--- src/pages/findinfo/FindInfo.js
+++ src/pages/findinfo/FindInfo.js
@@ -1,97 +1,115 @@
 import React, { useState } from 'react';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
-import { PiUserCircle } from 'react-icons/pi';
-import { MdAlternateEmail } from 'react-icons/md';
-import styled from 'styled-components';
 import { StyledLogo, RedLetter } from '../../components/Logo';
-
-const PageContainer = styled.div`
-  padding: 20px;
-  font-family: Arial, sans-serif;
-`;
+import { PiUserCircle } from 'react-icons/pi';
+import { CiMail } from 'react-icons/ci';
+import { GlobalStyle, FindIdContainer, FindPasswordContainer } from './FindInfoStyle';
 
 // FindInfo 컴포넌트
 const FindInfo = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [id, setId] = useState('');
+  const [nameForId, setNameForId] = useState('');
+  const [emailForId, setEmailForId] = useState('');
+
+  const [nameForPassword, setNameForPassword] = useState('');
+  const [emailForPassword, setEmailForPassword] = useState('');
+  const [idForPassword, setIdForPassword] = useState('');
 
   // 폼 제출 핸들러
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // 폼 제출 로직
-    console.log(name, email, id);
+    console.log(nameForId, emailForId, nameForPassword, emailForPassword, idForPassword);
 
     // console.log대신 API호출
     // 예: await axios.post('/api/userinfo', { username, email });
 
     // 입력 필드 초기화
-    setName('');
-    setEmail('');
-    setId('');
+    setNameForId('');
+    setEmailForId('');
+    setNameForPassword('');
+    setEmailForPassword('');
+    setIdForPassword('');
   };
 
   return (
-    <PageContainer>
+    <>
+      <GlobalStyle />
       <form onSubmit={handleSubmit}>
         <StyledLogo>
           IDE<RedLetter>A</RedLetter>
         </StyledLogo>
 
-        <h2>아이디 찾기</h2>
-        <div className='input-name'>
-          <PiUserCircle /> 성명
-          <Input
-            label='성명'
-            placeholder=' 예시) 홍길동'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className='input-email'>
-          <MdAlternateEmail /> 이메일
-          <Input
-            label='이메일'
-            placeholder=' 가입한 이메일 입력'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <Button type='submit'>찾기</Button>
+        <FindIdContainer>
+          <div>
+            <h2>아이디 찾기</h2>
+            <div className='input-find-id'>
+              <p>
+                <PiUserCircle />
+                성명
+              </p>
+              <Input
+                label='성명'
+                placeholder=' 예시) 홍길동'
+                value={nameForId}
+                onChange={(e) => setNameForId(e.target.value)}
+              />
+              <p>
+                <CiMail />
+                이메일
+              </p>
+              <Input
+                label='이메일'
+                placeholder=' 가입한 이메일 입력'
+                value={emailForId}
+                onChange={(e) => setEmailForId(e.target.value)}
+              />
+              <Button type='submit'>찾기</Button>
+            </div>
+          </div>
+        </FindIdContainer>
 
-        <h2>비밀번호 찾기</h2>
-        <div className='input-name'>
-          <PiUserCircle /> 성명
-          <Input
-            label='성명'
-            placeholder=' 예시) 홍길동'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className='input-email'>
-          <MdAlternateEmail /> 이메일
-          <Input
-            label='이메일'
-            placeholder=' 가입한 이메일 입력'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className='input-id'>
-          <PiUserCircle /> 아이디
-          <Input
-            label='아이디'
-            placeholder=' 찾을 아이디 입력'
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-          />
-        </div>
-        <Button type='submit'>찾기</Button>
+        <FindPasswordContainer>
+          <div>
+            <h2>비밀번호 찾기</h2>
+            <div className='input-find-password'>
+              <p>
+                <PiUserCircle />
+                성명
+              </p>
+              <Input
+                label='성명'
+                placeholder=' 예시) 홍길동'
+                value={nameForPassword}
+                onChange={(e) => setNameForPassword(e.target.value)}
+              />
+              <p>
+                <CiMail />
+                이메일
+              </p>
+              <Input
+                label='이메일'
+                placeholder=' 가입한 이메일 입력'
+                value={emailForPassword}
+                onChange={(e) => setEmailForPassword(e.target.value)}
+              />
+              <p>
+                <PiUserCircle />
+                아이디
+              </p>
+              <Input
+                label='아이디'
+                placeholder=' 찾을 아이디 입력'
+                value={idForPassword}
+                onChange={(e) => setIdForPassword(e.target.value)}
+              />
+              <Button type='submit'>찾기</Button>
+            </div>
+          </div>
+        </FindPasswordContainer>
       </form>
-    </PageContainer>
+    </>
   );
 };
 
