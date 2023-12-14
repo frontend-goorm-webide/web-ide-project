db1f88e9a827925b6b7e946145a5547b78bbd2d2
jhrogh <wogml5031@gmail.com>
2023-12-14 12:14:35 +0900

    주석 추가

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; // React Router에서 useNavigate 가져오기
+import { useNavigate } from 'react-router-dom';
 import Button from './Button';
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const CommonModal = ({ title, contents, btnName, closeModal, redirectTo }) => {
+  // 훅
   const navigate = useNavigate();
 
+  // 버튼 클릭 시, 페이지 이동 및 모달 닫기
   const handleButtonClick = () => {
     if (redirectTo) {
-      navigate(redirectTo); // 버튼 클릭 시 페이지 이동
+      navigate(redirectTo); // 버튼 클릭 시 전달받은 페이지 이동
     }
     closeModal(); // 모달 닫기
   };
@@ -17,7 +19,7 @@ const CommonModal = ({ title, contents, btnName, closeModal, redirectTo }) => {
   return (
     <div>
       {/* isOpen은 항상 true : 모달이 항상 열려있음
-      toggle : 모달 열고 닫는 것을 수행하는 함수
+      toggle : 모달 열고 닫는 것을 수행하는 함수 (reractstrap 정의)
       toggle={closeModal} : toggle 속성을 closeModal 함수로 설정 */}
       <Modal isOpen={true} toggle={closeModal}>
         <ModalHeader toggle={closeModal}>{title}</ModalHeader>
