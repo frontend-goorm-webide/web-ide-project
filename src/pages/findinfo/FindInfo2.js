2f0d76d482481a6465d5613c03958345778ba22d
jhrogh <wogml5031@gmail.com>
2023-12-14 17:10:15 +0900

    비밀번호재설정 modal 별도 추가/FindInfo.js 버튼 클릭 시 Modal.js 호출/Modal 가운데 정렬/기타

--- src/pages/findinfo/FindInfo2.js
+++ src/pages/findinfo/FindInfo2.js
@@ -5,8 +5,6 @@ import { StyledLogo, RedLetter } from '../../components/Logo';
 import { PiUserCircle } from 'react-icons/pi';
 import { CiMail } from 'react-icons/ci';
 import { GlobalStyle, FindIdContainer, FindPasswordContainer } from './FindInfoStyle';
-import CommonModal from '../../components/Modal';
-import NewPwModal from './NewPwModal';
 
 // FindInfo 컴포넌트
 const FindInfo = () => {
@@ -33,36 +31,6 @@ const FindInfo = () => {
     setNameForPassword('');
     setEmailForPassword('');
     setIdForPassword('');
-  };
-
-  // 모달 열고 닫기 -> false 초기화
-  const [isModalOpen, setModalOpen] = useState(false);
-  // 비밀번호재설정 모달 열고 닫기 -> false 초기화
-  const [isPwModalOpen, setPwModalOpen] = useState(false);
-  // 모달 내용 -> 빈내용 초기화
-  const [modalContent, setModalContent] = useState({});
-  // 비밀번호재설정모달 내용 -> 빈내용 초기화
-  const [pwModalContent, setPwModalContent] = useState({});
-
-  // 모달 열기 함수
-  const openModal = (content) => {
-    setModalOpen(true); // isModalOpen = true
-    setModalContent(content); // 모달 내용 전달
-  };
-  // 모달 닫기 함수
-  const closeModal = () => {
-    setModalOpen(false); // isModalOpen = false
-    setModalContent({}); // 모달 내용 공백 전달
-  };
-  // 모달 열기 함수 (비밀번호재설정 모달)
-  const openPwModal = (content) => {
-    setPwModalOpen(true); // isModalOpen = true
-    setPwModalContent(content); // 모달 내용 전달
-  };
-  // 모달 닫기 함수 (비밀번호재설정 모달)
-  const closePwModal = () => {
-    setPwModalOpen(false); // isModalOpen = false
-    setPwModalContent({}); // 모달 내용 공백 전달
   };
 
   return (
@@ -97,18 +65,7 @@ const FindInfo = () => {
                 value={emailForId}
                 onChange={(e) => setEmailForId(e.target.value)}
               />
-              <Button
-                onClick={() =>
-                  openModal({
-                    title: '아이디 찾기',
-                    contents: '해당 아이디로 다시 로그인 해주세요:)',
-                    btnName: '로그인하기',
-                    redirectTo: '/', // main 페이지 이동
-                  })
-                }
-              >
-                찾기
-              </Button>
+              <Button type='submit'>찾기</Button>
             </div>
           </div>
         </FindIdContainer>
@@ -147,22 +104,9 @@ const FindInfo = () => {
                 value={idForPassword}
                 onChange={(e) => setIdForPassword(e.target.value)}
               />
-              <Button
-                onClick={() =>
-                  openPwModal({
-                    title: '비밀번호 재설정',
-                    contents: '새로운 비밀번호로 로그인 해주세요 :)',
-                    btnName: '로그인하기',
-                    redirectTo: '/', // main 페이지 이동
-                  })
-                }
-              >
-                찾기
-              </Button>
+              <Button type='submit'>찾기</Button>
             </div>
           </div>
-          {isModalOpen && <CommonModal {...modalContent} closeModal={closeModal} />}
-          {isPwModalOpen && <NewPwModal {...pwModalContent} closePwModal={closePwModal} />}
         </FindPasswordContainer>
       </form>
     </>
