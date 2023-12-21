4e82d745a90fa473d6c272e766cb30fb88e47d0a
Jiyeong <tokyru@naver.com>
2023-12-21 15:00:42 +0900

    임시 데이터 주석처리

--- src/pages/ide/MyInfoModal.js
+++ src/pages/ide/MyInfoModal.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Modal } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import { data } from '../Data';
+//import { data } from '../Data';
 import {
   CenteredModalHeader,
   CenteredModalBody,
