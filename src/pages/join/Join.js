9c74cb7f6784a48a1e253631d4175c751d8d6607
Hyejung Na <hyejung.na01@gmail.com>
2023-12-22 00:22:02 +0900

    중복확인 버튼클릭시  유효성검사, 모달  추가

--- src/pages/join/Join.js
+++ src/pages/join/Join.js
@@ -34,11 +34,55 @@ const Join = () => {
     navigate('/');
   };
 
-  // 이메일 유효성 검사 함수
-  const validateEmail = (email) => {
-    // 영어 알파벳, 숫자 및 일부 특수문자(.-@)만 허용하는 정규식(한글입력불가)
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailRegex.test(email);
+  //아이디 중복확인 검사 함수
+  const handleIdDuplicationCheck = (event) => {
+    event.preventDefault();
+
+    if (id) {
+      openModal({
+        title: '중복 확인',
+        contents: <ErrorText>사용할수 있는 아이디입니다.</ErrorText>,
+        btnName: '닫기',
+        redirectTo: null,
+      });
+    }
+  };
+
+  const handlePwDuplicationCheck = (event) => {
+    event.preventDefault();
+
+    if (!validatePassword(password)) {
+      openModal({
+        title: '입력 오류',
+        contents: (
+          <ErrorText>
+            아이디는 4~10글자사이로, 특수문자를 제외한 영문 대문자 또는 소문자를 포함하고, 숫자를
+            반드시 포함하여야 합니다.
+          </ErrorText>
+        ),
+        btnName: '닫기',
+        redirectTo: null,
+      });
+    } else if (password !== confirmPassword) {
+      openModal({
+        title: '비밀번호 중복확인 오류',
+        contents: <ErrorText>비밀번호가 동일하지 않습니다. 다시 입력해 주세요.</ErrorText>,
+        btnName: '닫기',
+        redirectTo: null,
+      });
+    } else {
+      openModal({
+        title: '비밀번호 중복확인',
+        contents: (
+          <>
+            <p>비밀번호 중복확인이 되었습니다.</p>
+          </>
+        ),
+        btnName: '로그인하기',
+        redirectTo: null,
+      });
+      console.log('비밀번호 중복확인 성공');
+    }
   };
 
   // 성명 유효성 검사 함수
@@ -62,6 +106,14 @@ const Join = () => {
     return passwordRegex.test(password);
   };
 
+  // 이메일 유효성 검사 함수
+  const validateEmail = (email) => {
+    // 영어 알파벳, 숫자 및 일부 특수문자(.-@)만 허용하는 정규식(한글입력불가)
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  };
+
+  //휴대전화번호 유효성 검사 함수
   const validateMobileNumber = (mobile) => {
     const mobileNumberRegex = /^01\d{8,9}$/;
     return mobileNumberRegex.test(mobile);
@@ -228,7 +280,9 @@ const Join = () => {
                 value={id}
                 onChange={(e) => setId(e.target.value)}
               />
-              <CheckIdBtn type='submit'>중복 확인</CheckIdBtn>
+              <CheckIdBtn onClick={handleIdDuplicationCheck} type='submit'>
+                중복 확인
+              </CheckIdBtn>
             </IdInputWithBtn>
             <PwInputContainer>
               <span>
@@ -241,8 +295,8 @@ const Join = () => {
                       </p>
                     </>
                   }
-                  placeholder=' 영문 대/소문자, 숫자, 특수문자 각 1개
-                  포함.'
+                  placeholder=' 영어 대&소문자, 숫자, 특수문자 각 1개
+                  포함'
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
@@ -261,7 +315,9 @@ const Join = () => {
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
               />
-              <CheckIdBtn type='submit'>중복 확인</CheckIdBtn>
+              <CheckIdBtn onClick={handlePwDuplicationCheck} type='submit'>
+                중복 확인
+              </CheckIdBtn>
             </PwInputContainer>
             <Input
               label={
