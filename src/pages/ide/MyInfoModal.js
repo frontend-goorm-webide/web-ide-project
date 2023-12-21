5d38278a1289944901ada5024b651bc1fb588e5f
Jiyeong <tokyru@naver.com>
2023-12-21 12:56:51 +0900

    dev 병합

--- src/pages/ide/MyInfoModal.js
+++ src/pages/ide/MyInfoModal.js
@@ -1,119 +1,166 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Modal } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import { data } from '../Data';
+import {
+  CenteredModalHeader,
+  CenteredModalBody,
+  CenteredModalFooter,
+  SectionContainer,
+  ProfileImage,
+  Section,
+  ButtonContainer,
+  Button,
+} from './StyleMyInfoModal';
 
-const ModalWrapper = styled.div`
-width: 500px; 
-height: 300px; 
-`;
-
-const Section = styled.div`
-margin - bottom: 20px;
-`;
-
-const Button = styled.button`
-  width: 110px;
-  height: 40px;
-  background-color: #36599d;
-  color: white;
-  font-size: 18px;
-  font-weight: bold;
-  text-align: center;
-  border: 1px solid black;
-  border-radius: 15px;
-  cursor: pointer;
-  &:hover {
-    background-color: darkblue;
-  }
-  margin-top: 10px;
-`;
-
-const MyInfoModal = ({ onRequestClose }) => {
+// isMyInfoOpen : 내정보 모달 열기/닫기
+// closeMyInfo : ide.js 에서 closeMyInfoModal() 함수
+// open : ide.js 에서 openModal() 함수 -> 회원탈퇴(공통 모달)
+const MyInfoModal = ({ isMyInfoOpen, closeMyInfo, open }) => {
   // 상태 추가
+  const [profilePicture, setProfilePicture] = useState('');
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
-  // useNavigate 훅을 사용하여 페이지 이동 함수 가져오기
-  const navigate = useNavigate();
 
-  // 프로필 정보 변경 함수
-  const handleProfileChange = () => {
-    // 변경된 정보를 콘솔에 출력
-    console.log('Profile information changed:', {
-      profilePicture,
-      name,
-      email,
-      phone,
+  // data[1]의 값 가져와서 화면에 띄우기
+  useEffect(() => {
+    const userData = data[1];
+    setProfilePicture(userData.storedFileName);
+    setName(userData.name);
+    setUserId(userData.userId);
+    setEmail(userData.email);
+    setPhone(userData.phone);
+  }, []);
+
+  // 회원 탈퇴 버튼 클릭 시 처리
+  const clickRemoveButton = () => {
+    // ide.js 에서 openModal() 실행 -> 공통 Modal
+    open({
+      title: '회원탈퇴',
+      contents: '회원 탈퇴가 완료되었습니다.',
+      btnName: '확인',
+      redirectTo: '/',
     });
   };
+  // ===========================프로필 사진 변경===========================
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [previewImage, setPreviewImage] = useState(null);
 
-  // 회원 탈퇴 함수
-  const handleWithdrawal = () => {
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
 
-    // 회원 탈퇴 요청을 콘솔에 출력
-    console.log('Withdrawal requested');
-    // 회원 탈퇴 완료 시 알림창 표시
-    alert('회원 탈퇴가 완료되었습니다.');
-    // 모달 닫기
-    onRequestClose();
-    //메인 페이지로 이동
-    navigate('/');
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setPreviewImage(reader.result);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+    setSelectedImage(file);
   };
 
+  const handleImageUpload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('image', selectedImage);
 
+      const apiUrl = 'URL';
+
+      const response = await axios.post(apiUrl, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log('서버 응답:', response.data);
+    } catch (error) {
+      console.error('에러 발생:', error);
+    }
+  };
+  // ===========================api===========================
+
+  // 내정보 api 요청
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/api/users/my-info', {
+        headers: {
+          Token: 'token-value',
+        },
+      })
+      .then((response) => {
+        // 성공적으로 응답을 받았을 때의 처리
+        console.log('서버 응답:', response.data);
+
+        const userData = response.data[0];
+        setProfilePicture(userData.storedFileName);
+        setName(userData.name);
+        setUserId(userData.userId);
+        setEmail(userData.email);
+        setPhone(userData.phone);
+      })
+      .catch((error) => {
+        // 오류 발생 시의 처리
+        console.error('에러 발생:', error);
+      });
+  }, []);
+
+  // 회원탈퇴 api 요청
+  useEffect(() => {
+    axios
+      .put('http://localhost:8080/api/users/delete', {
+        headers: {
+          Token: 'token-value',
+        },
+      })
+      .then((response) => {
+        // 성공적으로 응답을 받았을 때의 처리
+        console.log('서버 응답:', response.data);
+
+        clickRemoveButton();
+      })
+      .catch((error) => {
+        // 오류 발생 시의 처리
+        console.error('에러 발생:', error);
+      });
+  }, []);
 
   return (
-    <ModalWrapper>
-      {/* 프로필 섹션 */}
-      <Section>
-        <img src='/Users/jiyeong/Documents/web-ide-frontend-main/src/pages/ide/user_picture.jpg' alt="기본 프로필 사진"></img>
-        <p>프로필 사진: {profilePicture}</p>
-        {/* 프로필 사진 변경 기능 */}
-        <input
-          type="file"
-          accept="image/*"
-          onChange={(e) => setProfilePicture(e.target.value)}
-        />
-        {/* 프로필 변경 버튼 */}
-        <Button onClick={handleProfileChange}>프로필 변경</Button>
-      </Section>
+    <div>
+      <Modal isOpen={isMyInfoOpen} toggle={closeMyInfo} backdrop={false}>
+        <CenteredModalHeader toggle={closeMyInfo}>내 정보</CenteredModalHeader>
 
-      {/* 가입 정보 섹션 */}
-      <Section>
-        <p>이름: {name}</p>
-        {/* 이름 변경 기능 */}
-        <input
-          type="text"
-          placeholder="이름"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+        <CenteredModalBody>
+          {/* 프로필 섹션 */}
+          <Section>
+            <SectionContainer>
+              <ProfileImage src={previewImage || profilePicture} alt='기본 프로필 사진' />
+              <p>프로필 사진</p>
+              {/* 프로필 사진 변경 기능 */}
+              <input type='file' accept='image/*' onChange={handleImageChange} />
+            </SectionContainer>
+            {/* 프로필 변경 버튼 */}
+            <ButtonContainer>
+              <Button onClick={handleImageUpload}>프로필 변경</Button>
+            </ButtonContainer>
+          </Section>
+          {/* 가입 정보 섹션 */}
+          <Section>
+            <p>이름: {name}</p>
+            <p>아이디: {userId}</p>
+            <p>이메일: {email}</p>
+            <p>휴대폰 번호: {phone}</p>
+          </Section>
+        </CenteredModalBody>
 
-        <p>아이디: {email}</p>
-        {/* 아이디 변경 기능 */}
-        <input
-          type="text"
-          placeholder="아이디"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <p>이메일: {phone}</p>
-        {/* 이메일 변경 기능 */}
-        <input
-          type="text"
-          placeholder="이메일"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-        />
-      </Section>
-
-      {/* 닫기 버튼 */}
-      <Button onClick={onRequestClose}>닫기</Button>
-
-      {/* 회원 탈퇴 버튼 */}
-      <Button onClick={handleWithdrawal}>회원 탈퇴</Button>
-    </ModalWrapper>
+        <CenteredModalFooter>
+          {/* 회원 탈퇴 버튼 ==> 이벤트 삭제 */}
+          <Button onClick={clickRemoveButton}>회원탈퇴</Button>
+        </CenteredModalFooter>
+      </Modal>
+    </div>
   );
 };
 
