97408347db70bab82076736ef1089441cf3a9a1b
Hyejung Na <hyejung.na01@gmail.com>
2023-12-22 14:01:14 +0900

    유저정보 db 공통수정

--- src/pages/Data.js
+++ src/pages/Data.js
@@ -1,41 +1,31 @@
 export const data = [
   {
-    userId: 'dog88',
-    pwd: '123123',
-    name: 'jeon1',
-    email: 'test1@naver.com',
-    phone: '010-9999-9999',
-    storedFileName: 'person1.png',
-    code: 'test1가 입력한 코드',
-    messageType: 'ENTER',
-    text: 'test1님이 입장하셨습니다.',
-    sendDate: '', // sendDate 정보가 빈 문자열로 제공되어 있습니다. 필요에 따라서 채워 넣으셔도 됩니다.
-    roomId: 'room_id',
+    userId: 'testid1',
+    pwd: 'Rnfma1234!', // 구름1234! (r은 대문자)
+    name: '김구름',
+    email: 'test1@gmail.com',
+    phone: '01012345678',
+    storedFileName: 'person1.png', // 프로필 사진
   },
   {
-    userId: 'cat88',
-    pwd: '12341234',
-    name: 'jeon2',
-    email: 'test2@naver.com',
-    phone: '010-9999-9999',
-    storedFileName: '/img/kakaoLogin.png',
-    code: 'test2가 입력한 코드',
-    messageType: 'TALK',
-    text: 'text내용',
-    sendDate: '', // sendDate 정보가 빈 문자열로 제공되어 있습니다. 필요에 따라서 채워 넣으셔도 됩니다.
-    roomId: 'room_id',
+    userId: 'testid2',
+    pwd: 'Rnfma5678!', // 구름1234! (r은 대문자)
+    name: '똑바로',
+    email: 'test2@gmail.com',
+    phone: '01087654321',
+    storedFileName: 'person2.png', // 프로필 사진
   },
-  {
-    userId: 'hamster88',
-    pwd: '12351235',
-    name: 'jeon3',
-    email: 'test3@naver.com',
-    phone: '010-9999-9999',
-    storedFileName: 'person3.png',
-    code: 'test3가 입력한 코드',
-    messageType: 'EXIT',
-    text: 'test3님이 퇴장하셨습니다.',
-    sendDate: '', // sendDate 정보가 빈 문자열로 제공되어 있습니다. 필요에 따라서 채워 넣으셔도 됩니다.
-    roomId: 'room_id',
-  },
+  // {
+  //   userId: 'hamster88',
+  //   pwd: '12351235',
+  //   name: 'jeon3',
+  //   email: 'test3@naver.com',
+  //   phone: '010-9999-9999',
+  //   storedFileName: 'person3.png',
+  //   code: 'test3가 입력한 코드',
+  //   messageType: 'EXIT',
+  //   text: 'test3님이 퇴장하셨습니다.',
+  //   sendDate: '', // sendDate 정보가 빈 문자열로 제공되어 있습니다. 필요에 따라서 채워 넣으셔도 됩니다.
+  //   roomId: 'room_id',
+  // },
 ];
